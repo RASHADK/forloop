7c46e03bc6baded97628a5a25b78d7fcb12393f4
RASHADK <110896723+RASHADK@users.noreply.github.com>
2022-08-09 15:16:45 +0530

    updated

---
+++ forlist.py
@@ -0,0 +1,10 @@
+for_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+for x in for_list:
+    print(x)
+    print(x * x)
+    print(x * x * x)
+    print(x * x * x * x)
+print(".............................")
+new = ["thanha", 23, "place", "luminar"]
+for i in range(0, len(new)):
+    print(new[i])
